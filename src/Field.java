428f4b1506a2c7ccff1b28597abbcebbb141bacd
ERIKS MARKEVICS <erick.markevich@gmail.com>
2020-10-14 20:08:42 +0200

    problem 1 solved

--- src/Field.java
+++ src/Field.java
@@ -5,19 +5,26 @@
 //Hans Henrik Lovengreen     Oct 2, 2020
 
 public class Field {
-
+	int h = 11;
+	int w = 12;
+	Semaphore field[][];
     public Field() {
-
+    	field = new Semaphore[h][w];
+    	for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				field[i][j] = new Semaphore(1);
+			}
+		}
     }
 
     /* Block until car no. may safely enter tile at pos */
     public void enter(int no, Pos pos) throws InterruptedException {
-
+    	field[pos.row][pos.col].P();
     }
 
     /* Release tile at position pos */
     public void leave(Pos pos) {
-
+    	field[pos.row][pos.col].V();
     }
 
 }
